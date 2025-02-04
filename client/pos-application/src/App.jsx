7a750c91b16d606daf476898338864deefa9d30b
Muhammed Beraat Kildaci <muhammedberaatkildaci@gmail.com>
2025-02-04 21:42:51 +0300

    header component has been done

---
+++ client/pos-application/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ client/pos-application/src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { Input, Badge } from "antd";
+import {
+  SearchOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  CopyOutlined,
+  UserOutlined,
+  BarChartOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center gap-5 text-white border-b border-gray-200 px-4 py-3 mb-6 bg-green-700">
+      <div className="logo">
+        <a href="/">
+          <h2 className="text-2xl font-bold md:text-4xl">POS App</h2>
+        </a>
+      </div>
+      <div className="search flex-1">
+        <Input
+          className="rounded-full max-w-[800px]"
+          size="large"
+          placeholder="Ürün Ara"
+          prefix={<SearchOutlined />}
+        />
+      </div>
+      <div className="menu-links flex justify-between items-center gap-7 md:static fixed bottom-0 md:w-auto w-screen md:bg-transparent bg-green-700 left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
+        <a className="flex flex-col items-center hover:text-[#40a9ff] md:text-xl text-sm" href="/">
+          <HomeOutlined /> <span>Anasayfa</span>
+        </a>
+        <a className="flex flex-col items-center hover:text-[#40a9ff] md:text-xl text-sm" href="/">
+          <ShoppingCartOutlined /> <span>Sepet </span><Badge count={10} size="small" offset={[15, -50]} />
+        </a>
+        <a className="flex flex-col items-center hover:text-[#40a9ff] md:text-xl text-sm" href="/">
+          <CopyOutlined /> <span>Faturalar</span>
+        </a>
+        <a className="flex flex-col items-center hover:text-[#40a9ff] md:text-xl text-sm" href="/">
+          <UserOutlined /> <span>Kullanıcılar</span>
+        </a>
+        <a className="flex flex-col items-center hover:text-[#40a9ff] md:text-xl text-sm" href="/">
+          <BarChartOutlined /> <span>İstatistikler</span>
+        </a>
+        <a className="flex flex-col items-center hover:text-[#40a9ff] md:text-xl text-sm" href="/">
+          <LogoutOutlined /> <span>Çıkış</span>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
